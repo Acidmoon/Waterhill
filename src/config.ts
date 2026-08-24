9b0650ef8731c23ba6fc5096c33d5a260e0adead
Acidmoon <acidmoon@users.noreply.github.com>
2026-08-25 02:29:26 +0800

    feat: 主页大背景改为 Bing 每日壁纸,每天北京时间 8 点自动更新

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/hero.svg",
+		src: "assets/images/bing-daily.jpg",
 		position: "center",
 		credit: {
 			enable: false,
