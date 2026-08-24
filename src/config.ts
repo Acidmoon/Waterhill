010a590b3da0379eb2275af42515ae5f745ad194
Acidmoon <acidmoon@users.noreply.github.com>
2026-08-25 02:35:16 +0800

    feat: 换新头像(上半身方图 512x512)

--- src/config.ts
+++ src/config.ts
@@ -47,7 +47,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.svg",
+	avatar: "assets/images/avatar.png",
 	name: "Acidmoon",
 	bio: "口述文章为主 · 技术笔记与生活随笔",
 	links: [
